84cbdae45c91224c108492fcb69a187aecabe662
virginia.taisne@gmail.com <virginia.taisne@gmail.com>
2025-11-04 14:01:25 +0000

    ss

---
+++ models/staging/row/stg_row__sales.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('row', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        pdt_id AS product_id,
+        revenue,
+        quantity
+
+    from source
+
+)
+
+select * from renamed
